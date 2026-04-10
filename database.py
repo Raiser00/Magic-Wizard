d647ff51c4aaad135acf6e99e2bd2e564f44ec52
Raiser00 <damien.khashan@ifosup.wavre.be>
2026-04-10 17:48:27 +0200

    Creation de 4 monstres pour les tests

---
+++ database.py
@@ -0,0 +1,23 @@
+# Le dictionnaire de référence (Le catalogue des cartes existantes)
+CATALOGUE_MONSTRES = {
+    "Dragon Blanc aux Yeux Bleus": {
+        "atk": 3000, 
+        "def": 2500, 
+        "rank": 8
+    },
+    "Magicien Sombre": {
+        "atk": 2500, 
+        "def": 2100, 
+        "rank": 7
+    },
+    "Gardien Celte": {
+        "atk": 1400, 
+        "def": 1200, 
+        "rank": 4
+    },
+    "Kuriboh": {
+        "atk": 300, 
+        "def": 200, 
+        "rank": 1
+    }
+}

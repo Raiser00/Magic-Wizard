d8bdf1b2ee0e2def25398a817c4883b784c6b8ef
Raiser00 <damien.khashan@ifosup.wavre.be>
2026-04-10 17:40:59 +0200

    Implemention du scaling Atk Deff + modif character

--- entities.py
+++ entities.py
@@ -8,7 +8,9 @@ class Character:
         self.level = 1
         self.exp = 0
         self.position = [100, 100]
+        self.emptyCard = 5
         self.deck = []
+        self.ActiveCard = None
 
     def gainExp(self, amount):
         self.exp += amount
@@ -24,3 +26,28 @@ class Card:
         self.deff = defense
     def __str__(self):
         return f"{self.name} (ATK: {self.atk}, DEF: {self.deff})"
+
+class Monster:
+    def __init__(self, name, baseAtk, baseDeff, baseRank, actualLevel=1):
+        self.name = name
+        self.baseAtk = baseAtk
+        self.baseDeff = baseDeff
+        self.baseRank = baseRank
+        self.actualLevel = actualLevel
+
+        self.healthMax = baseRank * 100
+        self.health = self.healthMax
+
+
+    @property
+    def atk(self):
+        value = (self.baseAtk / self.baseRank) * self.actualLevel
+        return int(value)
+
+    @property
+    def deff(self):
+        value = (self.baseDeff / self.baseRank) * self.actualLevel
+        return int(value)
+    
+    def __str__(self):
+        return f"{self.name} (Niv.{self.actualLevel}/{self.baseRank}) ATK:{self.atk} DEF:{self.deff}"
